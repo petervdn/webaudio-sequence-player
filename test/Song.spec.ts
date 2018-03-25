79e802b4f322db1c31edbefaa9a3e9fe26a9a35d
Peter van der Noord <peterdunord@gmail.com>
2018-03-25 22:14:22 +0200

    Add simple song construct test

--- test/Song.spec.ts
+++ test/Song.spec.ts
@@ -1,7 +1,14 @@
 import {expect} from 'chai';
+import Song from "../src/lib/Song";
 
 describe('Song', () => {
-  it('should pass', () => {
-    expect(true).to.equal(true);
+  it('should construct', () => {
+    const song = new Song(120);
+    expect(song.bpm).to.equal(120);
+  });
+
+  it('should add a sequence', () => {
+    const song = new Song(120);
+    expect(song.bpm).to.equal(120);
   });
 });
